704e99134cd16aa1604014bb09945addc19ee275
Kohkiota <53455201+Kohkiota@users.noreply.github.com>
2020-12-07 01:08:50 +0900

    とりあえずトップページviewOKとする。細かいCssは機能面が完成してから、調整する方向で。

--- resources/views/layouts/myapp.blade.php
+++ resources/views/layouts/myapp.blade.php
@@ -3,27 +3,32 @@
     <title>@yield('title')</title>
     <style>
     body { font-size:16pt; color:#999; margin: 5px; }
-    h1 { font-size:50pt; text-align:left; color:#f6f6f6; margin:0px 0px 0px 0px; letter-spacing:-4pt; background-color:#5a24c5;}
     ul { font-size:12pt; }
     hr { margin: 25px 100px; border-top: 1px dashed #ddd; }
     .menutilte { font-size:14pt; font-weight:bold; margin: 0px; }
     .contnet { margin:10px; }
-    .footer { text-align:right; font-size:10pt; padding:10px 10px; border-bottom:solid 1px #ccc; color:#ccc; background-color:#5a24c5; }
-    .logo { font-size: 50pt; border: 5px bold; border-color:#fff; border-radius: 5px; text-align:left; color:#aaa; margin:0px 15px; }
+    .footer { text-align:right; font-size:10pt; margin: 0px -5px 0px -5px; padding:10px 10px; border-bottom:solid 1px #ccc; color:#fff; background-color:#0467e2bf; }
+
+    header { text-align:left;  padding: 8px 0px; margin:-5px -5px 0px -5px;  background-color:#0467e2bf; }
+    .logo { font-size: 15pt; font-weight:bold; color:#04e2ce; padding:0px 5px; margin:0 5; border: 5px solid; border-color:#fff; border-radius: 50px; vertical-align: middle;}
+    .title { font-size: 15pt; font-weight:bold; color:#f6f6f6; letter-spacing:0pt; margin:0px 0px; }
     </style>
 </head>
 <body>
-    <span class="logo" >M</span><br>
-    <h1>@yield('title')</h1>
+    <!-- <span class="logo" >M</span><br> -->
+    <header><span class="logo" >M</span><span class="title">@yield('title')</span></header>
+
     @section('menubar')
-    <h2 class="mentutitle">※メニュー</h2>
+    <!-- <h2 class="mentutitle">※メニュー</h2>
     <ul>
         <li>@show</li>
     </ul>
     <hr size="1">
-    <div class="content">
+    <div class="content"> -->
+    @show
     @yield('content')
     </div>
+
     <div class="footer">
     @yield('footer')
     </div>
--- resources/views/myapp/index.blade.php
+++ resources/views/myapp/index.blade.php
@@ -1,15 +1,24 @@
 @extends('layouts.myapp')
+<style>
+    .btnarea { overflow:hidden; text-align:center; margin: 10px 10px; width:100%;}
+    .btn {display: inline-block;  font-weight:bold; border:3px solid; text-decoration:none; }
+    .memobtn { font-size:80px; width: 100%;}
+    .halfbtna { float:left; font-size:40px; width: 45%; margin: 10px 0px 0px;}
+    .halfbtnb { float:right; font-size:40px; width: 45%; margin: 10px 0px 0px;}
+</style>
+@section('title', 'トップページ')
 
-@section('title', 'Index')
-
-@section('menubar')
+<!-- @section('menubar')
     @parent
     インデックスページ
-@endsection
+@endsection -->
 
 @section('content')
-    <p>ここが本文のコンテンツです。</p>
-    <p>必要なだけ記述できます。</p>
+    <div class="btnarea">
+        <a class="btn memobtn" href="myapp">MEMO</a><br>
+        <a class="btn halfbtna" href="myapp/add">新規メモ</a>
+        <a class="btn halfbtnb" href="myapp/list">メモ一覧</a>
+    </div>
 @endsection
 
 @section('footer')
