7571d6c16c01afc8af4abbdb72284fc2bfd30346
Kohkiota <53455201+Kohkiota@users.noreply.github.com>
2020-12-10 00:23:32 +0900

    cssをpublic/cssに移行した

--- resources/views/layouts/myapp.blade.php
+++ resources/views/layouts/myapp.blade.php
@@ -1,51 +1,7 @@
 <html>
 <head>
     <title>@yield('title')</title>
-    <style>
-        body {
-            margin: 0px 5px;
-        }
-
-        .headfoot {
-            display:flex;
-            align-items: center;
-            margin: 0px -5px;
-            padding: 5px 5px;
-            background-color:#0467e2bf;
-            color:#fff;
-        }
-
-        header {
-            justify-content: flex-start;
-        }
-
-        footer {
-            justify-content: flex-end;
-            font-size:15px;
-            margin-left: 50px;
-        }
-        .logo {
-            font-size: 20px;
-            font-weight:bold;
-            color:#04e2ce;
-            padding:0px 5px;
-            margin:0px 0px;
-            border: 5px solid;
-            border-color:#fff;
-            border-radius: 50px;
-            vertical-align: middle;
-        }
-
-        .title {
-            font-size: 25px;
-            font-weight: bold;
-            color: #f6f6f6;
-            letter-spacing: 0pt;
-            margin:0px 0px;
-        }
-
-
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/myapp.styles.css') }}">
 </head>
 <body>
     <div>
--- resources/views/myapp/index.blade.php
+++ resources/views/myapp/index.blade.php
@@ -1,44 +1,6 @@
+<link rel="stylesheet" href="{{ asset('css/index.styles.css') }}">
 @extends('layouts.myapp')
-<style>
-    .btn {
-        display:block;
-        font-weight: bold;
-        border: 3px solid;
-        text-decoration: none;
-    }
 
-    .btnarea {
-        margin:10px 0px;
-        width:100%;
-        text-align:center;
-    }
-
-    .btnareaone {
-        width:100%;
-    }
-
-    .btnareatwo {
-        display:flex;
-        justify-content:center;
-        width:100%;
-        margin-top: 10px;
-    }
-
-    .memobtn {
-        font-size:80px;
-        width: 100%;
-    }
-    .halfbtna {
-        font-size:40px;
-        width: 50%;
-        margin-right:10px;
-    }
-    .halfbtnb {
-        font-size:40px;
-        width: 50%;
-    }
-
-</style>
 @section('title', 'トップページ')
 
 @section('content')
