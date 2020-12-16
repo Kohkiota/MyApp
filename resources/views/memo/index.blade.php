3e9eaf00064de1df2ca025b88595a4b0c236e6c8
Kohkiota <53455201+Kohkiota@users.noreply.github.com>
2020-12-16 16:02:26 +0900

    一覧表示がエラーでなかなか完成しなかった原因は、foreachの中に$items！正しくは$item

---
+++ resources/views/memo/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.myapp')
+
+@section('title', 'Memo.index')
+
+@section('content')
+    <table>
+    <tr><th>Name</th><th>Mail</th><th>Age</th><th>title</th><th>contents</th></tr>
+    @foreach($items as $item)
+        <tr>
+            <td>{{ optional($item)->name }}</td>
+            <td>{{ optional($item)->age }}</td>
+            <td>{{ optional($item)->mail }}</td>
+            <td>{{ optional($item)->title }}</td>
+            <td>{{ optional($item)->contents }}</td>
+        </tr>
+    @endforeach
+    </table>
+@endsection
+
+@section('footer', 'myouga myouga')
