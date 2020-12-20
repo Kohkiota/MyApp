fd6ef472e9764dd7302563a116a73fc6988fb8c9
Kohkiota <53455201+Kohkiota@users.noreply.github.com>
2020-12-21 00:11:50 +0900

    一覧表示をmyapp/listに一元化し、表示形式を修正。

--- resources/views/myapp/list.blade.php
+++ resources/views/myapp/list.blade.php
@@ -1,13 +1,17 @@
 @extends('layouts.myapp')
 
-@section('title', 'Memo.index')
+@section('title', 'メモ一覧')
 
 @section('content')
     <table>
-    <tr><th>Data</th></tr>
+    <tr><th>Name</th><th>Mail</th><th>Age</th><th>title</th><th>contents</th></tr>
     @foreach($items as $item)
         <tr>
-            <td>{{ $item->getData() }}</td>
+            <td>{{ optional($item)->name }}</td>
+            <td>{{ optional($item)->age }}</td>
+            <td>{{ optional($item)->mail }}</td>
+            <td>{{ optional($item)->title }}</td>
+            <td>{{ optional($item)->contents }}</td>
         </tr>
     @endforeach
     </table>
