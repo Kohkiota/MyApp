260f969fcff5593eecd841b7a7501157ca7438b0
Kohkiota <53455201+Kohkiota@users.noreply.github.com>
2020-12-20 22:18:27 +0900

    listを追加

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Memo;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Illuminate\Support\Facades\DB;
 
 class MemoController extends Controller
@@ -12,5 +13,23 @@ class MemoController extends Controller
     {
         $items = Memo::all();
         return view('memo.index', ['items' => $items]);
+    }
+
+    public function list()
+    {
+        $items = Memo::all();
+        return view('myapp.list', ['items' => $items]);
+    }
+
+    public function find(Request $request)
+    {
+        return view('memo.find', ['input' => '']);
+    }
+
+    public function search(Request $request)
+    {
+        $item = Memo::find($request->input);
+        $param = ['input' => $request->input, 'item' -> $item];
+        return view('memo.find', $param);
     }
 }
