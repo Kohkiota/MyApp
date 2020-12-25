42c2106a6fbb7514c414c5f24b3091697d2dc93a
Kohkiota <53455201+Kohkiota@users.noreply.github.com>
2020-12-25 21:18:10 +0900

    seeder修正DB:→DB::のミス

--- database/seeders/MemosTableSeeder.php
+++ database/seeders/MemosTableSeeder.php
@@ -22,6 +22,8 @@ class MemosTableSeeder extends Seeder
             'title' => '大田大輔の初めてのメモ',
             'contents' => '本日はじめてこのサービスを利用しました。粗削りですが、スッキリと機能が制限されていてよいと思います。',
         ];
+        DB::table('memos')->insert($param);
+
         $param = [
             'name' => '大輔kssssss',
             'age' => '6',
@@ -30,6 +32,8 @@ class MemosTableSeeder extends Seeder
             'title' => '初めてのメモ',
             'contents' => '本日は制限されていてよいと思います。',
         ];
+        DB::table('memos')->insert($param);
+
         $param = [
             'name' => 'matumaru daigo',
             'age' => '4',
@@ -38,6 +42,8 @@ class MemosTableSeeder extends Seeder
             'title' => 'メモ',
             'contents' => 'よいと思います。',
         ];
+        DB::table('memos')->insert($param);
+
         $param = [
             'name' => '佐野ラーメン',
             'age' => '1',
@@ -46,6 +52,8 @@ class MemosTableSeeder extends Seeder
             'title' => '大田大輔の初めてのメモ',
             'contents' => '本日はじめてこのサービスを利用しました。',
         ];
+        DB::table('memos')->insert($param);
+
         $param = [
             'name' => 'kumakura masaru',
             'age' => '7',
@@ -54,6 +62,8 @@ class MemosTableSeeder extends Seeder
             'title' => '大田大輔メモ',
             'contents' => 'ステマ？',
         ];
+        DB::table('memos')->insert($param);
+
         $param = [
             'name' => 'だいご',
             'age' => '9',
@@ -62,6 +72,8 @@ class MemosTableSeeder extends Seeder
             'title' => '大モ',
             'contents' => 'dennkik電気屋さん',
         ];
+        DB::table('memos')->insert($param);
+
         $param = [
             'name' => '大田',
             'age' => '2',
@@ -70,6 +82,8 @@ class MemosTableSeeder extends Seeder
             'title' => '大田大輔',
             'contents' => 'サービス残業。',
         ];
+        DB::table('memos')->insert($param);
+
         $param = [
             'name' => '阿部大樹',
             'age' => '5',
@@ -78,6 +92,8 @@ class MemosTableSeeder extends Seeder
             'title' => 'Ubi3e',
             'contents' => 'おまちしています。',
         ];
+        DB::table('memos')->insert($param);
+
         $param = [
             'name' => 'あ',
             'age' => '6',
@@ -86,6 +102,8 @@ class MemosTableSeeder extends Seeder
             'title' => 'あメモ',
             'contents' => 'あ',
         ];
+        DB::table('memos')->insert($param);
+
         $param = [
             'name' => '１０',
             'age' => '8',
@@ -94,6 +112,8 @@ class MemosTableSeeder extends Seeder
             'title' => 'd;a;fjk;lasd',
             'contents' => '本10',
         ];
+        DB::table('memos')->insert($param);
+
         $param = [
             'name' => '大正百歳太郎',
             'age' => '2',
@@ -102,7 +122,7 @@ class MemosTableSeeder extends Seeder
             'title' => 'わしは大正時代から生きてきた老害',
             'contents' => '若者はわしのいうことを聞いておけばよいのじゃ。年寄りを敬え。私は老害だ。',
         ];
+        DB::table('memos')->insert($param);
 
-        DB:table('people')->insert($param);
     }
 }
