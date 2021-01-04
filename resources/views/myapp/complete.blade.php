f9192a03562d441b36cfd083af54e0c0a80bd0c9
Kohkiota <53455201+Kohkiota@users.noreply.github.com>
2021-01-04 16:50:42 +0900

    completeのcssをスタイルシートに分離

--- resources/views/myapp/complete.blade.php
+++ resources/views/myapp/complete.blade.php
@@ -1,51 +1,5 @@
+<link rel="stylesheet" href="{{ asset('css/complete.styles.css') }}">
 @extends('layouts.myapp')
-<style>
-    .btn {
-        display:block;
-        font-weight: bold;
-        border: 3px solid;
-        text-decoration: none;
-    }
-
-    .btnarea {
-        margin:10px 0px;
-        width:100%;
-        text-align:center;
-    }
-
-    .btnareazero {
-        width:100%;
-    }
-    .btnareaone {
-        display:flex;
-        justify-content:center;
-        width:100%;
-        margin-top: 10px;
-    }
-    .btnareatwo {
-        width:100%;
-        margin-top: 10px;
-    }
-
-    .okbtn{
-        font-size:80px;
-        width: 100%;
-    }
-    .memobtn{
-        font-size:80px;
-        width: 100%;
-    }
-    .halfbtna {
-        font-size:40px;
-        width: 50%;
-        margin-right:10px;
-    }
-    .halfbtnb {
-        font-size:40px;
-        width: 50%;
-    }
-
-</style>
 @section('title', '完了ページ')
 
 @section('content')
