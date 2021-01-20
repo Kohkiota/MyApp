81656ae2e3c7e03f58dca9c2ad219a044949d427
Kohkiota <53455201+Kohkiota@users.noreply.github.com>
2021-01-20 10:45:15 +0900

    不要な行削除

--- app/Models/Memo.php
+++ app/Models/Memo.php
@@ -8,18 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Memo extends Model
 {
     use HasFactory;
-    // public $timestamps = false;
 
     protected $guarded = ['id'];
-
-    // public static $rules = [
-    //     'name' => 'required',
-    //     'age' => 'integer|min:0|max:9',
-    //         // 0=0~10歳, 1=11~20歳, 2=21~30歳, 3=31~40歳, 4=41~50歳, 5=51~60歳, 6=61~70歳, 7=71~80歳, 8=81~90歳, 9=91~100歳,
-    //     'mail' => 'email',
-    //     'memotype' => 'integer|min:0|max:2',
-    //         // 0=Text, 1=ToDo, 2=CheckList
-    // ];
 
     public function getData()
     {
